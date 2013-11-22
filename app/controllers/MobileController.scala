bb9d7facb5d35fa53af69171a7d6111e80347d91
Swati <swati@knoldus.com>
2013-11-22 16:27:49 +0530

    Add the functionality of check status

---
+++ app/controllers/MobileController.scala
@@ -0,0 +1,87 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Form
+import play.api.data.Forms._
+import model.domains.Domain._
+import model.users.UserService
+import model.users._
+import java.io.File
+import play.api.libs.json.Json
+
+class MobileController(userService: UserServiceComponent) extends Controller {
+  //this(userService: UserServiceComponent)
+  val mobileregistrationform = Form(
+    mapping(
+      "username" -> nonEmptyText,
+      "mobileName" -> nonEmptyText,
+      "mobileModel" -> nonEmptyText,
+      "imeiMeid" -> nonEmptyText,
+      "purchaseDate" -> sqlDate("yyyy-MM-dd"),
+      "contactNo" -> number,
+      "email" -> email,
+      "description" -> nonEmptyText)(MobileRegisterForm.apply)(MobileRegisterForm.unapply))
+
+   val mobilestatus= Form(
+      mapping(
+         "imeiMeid" -> nonEmptyText )(MobileStatus.apply)(MobileStatus.unapply))
+         
+  def mobileRegistrationForm: Action[play.api.mvc.AnyContent] = Action { implicit request =>
+    Ok(views.html.mobileRegistrationForm(mobileregistrationform))
+  }
+
+  def mobileRegistration = Action(parse.multipartFormData) { implicit request =>
+    Logger.info("MobileRegistrationController:mobileRegistrationForm - Mobile registration.")
+    Logger.info("mobileregistrationform" + mobileregistrationform)
+    mobileregistrationform.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.mobileRegistrationForm(formWithErrors)),
+      mobileuser => {
+        Logger.info("MobileRegistrationController:mobileRegistration - found valid data.")
+
+        val regMobile = UserService.mobileRegistration(Mobile(mobileuser.username, mobileuser.mobileName,
+          mobileuser.mobileModel, mobileuser.imeiMeid, mobileuser.purchaseDate, mobileuser.contactNo,
+          mobileuser.email, mobileuser.description))
+
+        request.body.file("fileUpload").map { image =>
+          val imageFilename = image.filename
+          val contentType = image.contentType.get
+          image.ref.moveTo(new File("/home/gaurav/Desktop/" + mobileuser.imeiMeid))
+        }
+
+        regMobile match {
+          case Right(mobileuser) => {
+            Redirect(routes.Application.index)
+
+          }
+          case Left(message) =>
+            Redirect(routes.MobileController.mobileRegistrationForm).flashing("message" -> "error")
+        }
+      })
+
+  }
+  
+   def getImeiMeidList(imeid: String): Action[play.api.mvc.AnyContent] = Action {implicit request =>
+      Logger.info("MobileController: getImeiMeidList method has been called.")
+      val mobileData = userService.getMobileRecordByIMEID(imeid).head
+      Logger.info("Mobile Records" + mobileData)
+      implicit val resultWrites = Json.writes[model.domains.Domain.Mobile]
+       val obj = Json.toJson(mobileData)(resultWrites)
+       if(mobileData.id != None){
+         Logger.info("mobileData>>>>>>" +mobileData)
+         Ok(Json.obj("status" -> "Ok", "mobileData" -> obj))
+         //Ok(Json.toJson("success"))
+       }else {
+        Ok(Json.obj("status" -> "Error"))
+        // Ok(Json.toJson("error"))
+       }
+  }
+  
+  def mobileStatus:  Action[play.api.mvc.AnyContent] = Action {implicit request =>
+    Ok(views.html.mobileStatus(mobilestatus))   
+  }
+
+}
+
+object MobileController extends MobileController(UserService)
---
+++ app/model/domains/Domain.scala
@@ -0,0 +1,69 @@
+package model.domains
+
+import scala.slick.driver.PostgresDriver.simple._
+
+object Domain {
+
+  /**
+   *  Question  object and mapping with question table in database
+   *
+   */
+  case class Mobile(
+    username:String,
+    mobileName:String,
+    mobileModel:String,
+    imeiMeid:String,
+    purchaseDate: java.sql.Date,
+    contactNo:Int,
+    email: String,
+    description: String,
+    id: Option[Int] = None
+    )
+
+  object Mobiles extends Table[Mobile]("mobiles") {
+    def id: Column[Option[Int]] = column[Option[Int]]("id", O.PrimaryKey, O.AutoInc)
+    def username: Column[String] = column[String]("username", O.NotNull, O DBType ("VARCHAR(100)"))
+    def mobileName: Column[String] = column[String]("mobile_name", O.NotNull, O DBType ("VARCHAR(100)"))
+    def mobileModel: Column[String] = column[String]("mobile_model", O.NotNull, O DBType ("VARCHAR(100)"))
+    def imeiMeid: Column[String] = column[String]("imei_meid", O.NotNull, O DBType ("VARCHAR(100)"))
+    def purchaseDate: Column[java.sql.Date] = column[java.sql.Date]("purchase_date", O.NotNull)
+    def contactNo: Column[Int] = column[Int]("contact_no", O.NotNull)
+    def email: Column[String] = column[String]("email", O.NotNull, O DBType ("VARCHAR(100)"))
+    def description: Column[String] = column[String]("description", O.NotNull, O DBType ("VARCHAR(100)"))
+    
+    //def * : scala.slick.lifted.MappedProjection[MobileRegistration,(String,String,String,String,java.sql.Date,Int,String,String, Option[Int])]= username ~ mobileName ~ mobileModel ~ imeiMeid ~ purchaseDate ~ contactNo ~ email ~ description ~ id <> (MobileRegistration.apply _, MobileRegistration.unapply _)
+    
+    def * : scala.slick.lifted.MappedProjection[Mobile, (String, String, String, String, java.sql.Date, Int, String, String, Option[Int])] =
+      username ~ mobileName ~ mobileModel ~ imeiMeid ~ purchaseDate ~ contactNo ~ email ~ description ~ id <> (Mobile, Mobile unapply _)
+    
+    def insert: slick.driver.PostgresDriver.KeysInsertInvoker[Mobile, Option[Int]] =
+      username ~ mobileName ~ mobileModel ~ imeiMeid ~ purchaseDate ~ contactNo ~
+      email ~ description<> (
+        { (username,mobileName, mobileModel,imeiMeid, purchaseDate, contactNo ,  email , description) =>
+          Mobile(username,mobileName, mobileModel,imeiMeid, purchaseDate, contactNo ,  email , description)
+        },
+        { mobileregistration: Mobile =>
+          Some((mobileregistration.username,mobileregistration.mobileName, mobileregistration.mobileModel,mobileregistration.imeiMeid, mobileregistration.purchaseDate, mobileregistration.contactNo ,  mobileregistration.email , mobileregistration.description))
+        }) returning id
+  }
+  
+  
+  
+ 
+  case class MobileRegisterForm(
+      username:String,
+      mobileName:String,
+      mobileModel:String,
+      imeiMeid:String,
+      purchaseDate:java.sql.Date,
+      contactNo:Int,
+      email:String,
+      description:String
+     //id:Option[Int]=None
+    // fileUpload:java.io.File
+      
+  )
+  
+  case class MobileStatus(
+    imeiMeid:String)
+}
