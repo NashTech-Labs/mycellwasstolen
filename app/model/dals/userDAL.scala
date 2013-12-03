2a6cfefa47f6949b20f85f0efb3db9ecd5aafb4c
swati-knoldus <swati@knoldus.com>
2013-12-03 11:43:39 +0530

    Changes in service & dal class

---
+++ app/model/dals/userDAL.scala
@@ -0,0 +1,53 @@
+package model.dals
+
+import scala.slick.driver.PostgresDriver.simple._
+import scala.slick.session.Session
+
+import model.domains.Domain._
+import play.api.Logger
+import utils.Connection
+
+trait UserDALComponent {
+def insertMobileUser(mobileuser: Mobile): Either[String, Option[Int]]
+def getMobileRecordByIMEID(imeid: String): List[Mobile]
+def getMobilesName(): List[MobilesName]
+def getMobileModelsById(id: Int): List[MobileModels]
+}
+
+class UserDAL extends UserDALComponent {
+
+  override def insertMobileUser(mobileuser: Mobile): Either[String, Option[Int]] = {
+    try {
+      Connection.databaseObject().withSession { implicit session: Session =>
+        Right(Mobiles.insert.insert(mobileuser))
+      }
+    } catch {
+      case ex: Exception =>
+        Logger.info("Error in insert user" + ex.printStackTrace())
+        Left(ex.getMessage())
+    }
+  }
+  
+  override def getMobileRecordByIMEID(imeid: String): List[Mobile] = {
+      Connection.databaseObject().withSession { implicit session: Session =>
+        Logger.info("Calling getMobileRecordByIMEID" +imeid)
+       (for { mobile <- Mobiles if (mobile.imeiMeid === imeid) } yield mobile).list
+      }
+    }
+  
+  override def getMobilesName(): List[MobilesName] = {
+      Connection.databaseObject().withSession { implicit session: Session =>
+        Logger.info("Calling getMobilesName")
+       (for { mobilesName <- MobileName } yield mobilesName).list
+      }
+    }
+  
+  override def getMobileModelsById(id: Int): List[MobileModels] = {
+      Connection.databaseObject().withSession { implicit session: Session =>
+        Logger.info("Calling getMobileRecordByIMEID" +id)
+       (for { mobilemodel <- MobileModel if (mobilemodel.mobilesnameid === id) } yield mobilemodel).list
+      }
+    }
+}
+
+object UserDAL extends UserDAL
---
+++ app/model/users/userService.scala
@@ -0,0 +1,42 @@
+package model.users
+import model.domains.Domain._
+import play.api.Logger
+import model.dals._
+
+trait UserServiceComponent{
+  def mobileRegistration(mobileuser: Mobile): Either[String, Mobile]
+  def getMobileRecordByIMEID(imeid: String): Option[Mobile]
+  def getMobilesName(): List[MobilesName]
+  def getMobileModelsById(id: Int): List[MobileModels]
+}
+
+class UserService(userdal: UserDALComponent) extends UserServiceComponent{
+  
+  override def mobileRegistration(mobileuser: Mobile): Either[String, Mobile] = {
+    userdal.insertMobileUser(mobileuser) match {
+      case Right(id) => Right(Mobile(mobileuser.userName, mobileuser.mobileName,
+         mobileuser.mobileModel,mobileuser.imeiMeid,mobileuser.purchaseDate,mobileuser.contactNo,
+         mobileuser.email,mobileuser.description))
+      case Left(error) => Left(error)
+    }
+  }
+  
+  override def getMobileRecordByIMEID(imeid: String): Option[Mobile] = {
+    Logger.info("getMobileRecordByIMEID called")
+    val mobileData = userdal.getMobileRecordByIMEID(imeid)
+    if (mobileData.length != 0) Some(mobileData.head) else None
+  }
+  
+  override def getMobilesName(): List[MobilesName] = {
+    Logger.info("getMobilesName called")
+    userdal.getMobilesName()
+  }
+  
+  override def getMobileModelsById(id: Int): List[MobileModels] = {
+    Logger.info("getMobileRecordByIMEID called")
+    userdal.getMobileModelsById(id)
+  }
+
+}
+
+object UserService extends UserService(UserDAL)
