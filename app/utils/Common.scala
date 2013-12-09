791ff2ab1572e5c4e7b2d9b299220571658a8975
Swati <swati@knoldus.com>
2013-12-09 16:08:30 +0530

    Removing the scalastyle warnings

--- app/utils/Common.scala
+++ app/utils/Common.scala
@@ -5,15 +5,15 @@ import play.api.Play.current
 import play.api.i18n.Messages
 
 object Common {
-  
+
   def registerMessage(imeid: String): String = {
     Messages("messages.mobile.register", imeid, signature)
   }
-  
+
   def signature(): String = {
     Messages("messages.signature")
   }
-  
+
   def sendMail(email: String, subject: String, message: String): Unit = {
     val mail = use[MailerPlugin].email
     mail.setSubject(subject)
