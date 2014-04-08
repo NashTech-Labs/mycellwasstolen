5c191179619bd56dff609c78fa21bbd60b5cf3bb
Anand Singh <anand@knoldus.com>
2014-04-08 11:55:22 +0530

    Added code to populate default data.

--- app/Global.scala
+++ app/Global.scala
@@ -27,6 +27,9 @@ object Global extends GlobalSettings{
 
   override def onStart(app: Application): Unit = {
     Logger.info("Application has started")
+
+	//val result = model.convert.readcsv.convert
+
     try {
       Connection.databaseObject.withSession { implicit session: Session =>
       (Mobiles.ddl ++ Brands.ddl ++ MobileModel.ddl).create
@@ -36,6 +39,8 @@ object Global extends GlobalSettings{
     } catch {
       case ex: Exception => Logger.info(ex.getMessage() + ex.printStackTrace())
     }
+
+
   }
 
   override def onStop(app: Application): Unit = {
