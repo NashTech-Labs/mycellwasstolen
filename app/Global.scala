2b466623fd3eec3024cab1bfebcdfbd3ff522f42
Anand Singh <anand@knoldus.com>
2014-04-08 10:04:14 +0530

    Removed unwanted code

--- app/Global.scala
+++ app/Global.scala
@@ -26,15 +26,7 @@ object Global extends GlobalSettings{
   }
 
   override def onStart(app: Application): Unit = {
-    /*Logger.info("Application has started")
-    val bucketName = Play.application.configuration.getString("aws_bucket_name")
-    val accessKey = Play.application.configuration.getString("aws_access_key")
-    val secretKey = Play.application.configuration.getString("aws_secret_key")
-    val userId=  Play.application.configuration.getString("smtp.user")
-    val password= Play.application.configuration.getString("smtp.password")
-
-	val result = model.convert.readcsv.convert    
-
+    Logger.info("Application has started")
     try {
       Connection.databaseObject.withSession { implicit session: Session =>
       (Mobiles.ddl ++ Brands.ddl ++ MobileModel.ddl).create
@@ -43,9 +35,7 @@ object Global extends GlobalSettings{
       }
     } catch {
       case ex: Exception => Logger.info(ex.getMessage() + ex.printStackTrace())
-    }*/
-
-
+    }
   }
 
   override def onStop(app: Application): Unit = {
