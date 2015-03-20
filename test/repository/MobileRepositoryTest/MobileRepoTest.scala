ec23d54f3f764ab881bceb3e4547a569718e1e11
Manish <reseamanish@gmail.com>
2015-03-20 13:18:47 +0530

    Unit Test initiated in h2 database for MobileRepository and insertMobileUser tested for Mobile Repo

--- test/repository/MobileRepositoryTest/MobileRepoTest.scala
+++ test/repository/MobileRepositoryTest/MobileRepoTest.scala
@@ -25,11 +25,22 @@ class MobileRepoTest extends FunSuite with BeforeAndAfterEach with MobileReposit
     "gauravs", 1, 2, "12345678901234", "12345678902134", "12-05-2013", "+91 9839839830",
     "gs@gmail.com", "stolen", Status.pending, "ddas asd", "12-17-2013", "gaurav.png", "Sigma", "Sigma454", Some(1))
 
-  //Mobile Insertion Test
+  //Mobile Insertion Test 
   test("MobileRepository:insert and get mobile name successfully ") {
     running(FakeApplication()) {
       val insertedMobile = MobileRepository.insertMobileUser(mobileUser)
       assert(insertedMobile === Right(Some(1)))
+    }
+  }
+  
+  //Insert A User with Duplicate Email
+  test("MobileRepository:insert fails since email is duplicate ") {
+    running(FakeApplication()) {
+      //Insert a Mobile Record first
+      val insertedMobile = MobileRepository.insertMobileUser(mobileUser)
+      //Insert it again to test duplicate entry 
+      val insertedDuplicateMobile = MobileRepository.insertMobileUser(mobileUser)
+      assert(insertedDuplicateMobile.isLeft)
     }
   }
   
